b1794fe98ade3c23a7c5e576d3695e7f0901cb1b
jyothika <jyothikachitturi5@gmail.com>
2025-02-04 22:44:34 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import style from 'styled-components'
+
 import './App.css';
+import styled, { ThemeProvider} from "styled-components";
+import Navbar from "./components/Navbar";
+import Hero from "./" 
+import { darkTheme } from './util/utils';
+
+const Body = styled.div`
+   background-color: ${({ theme }) => theme.bg};
+   width: 100%;
+   height: 100%;
+   overflow-x: hidden;
+   `;
 
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+   <ThemeProvider theme={darkTheme}>
+    <Navbar/>
+    <body>
+      <Hero />
+      <Skills/>
+    </body>
+   </ThemeProvider>
   );
 }
 
