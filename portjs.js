c4cea1b90f0847eb8fd87cb2c6925de7c62eea25
jyothika <jyothikachitturi5@gmail.com>
2025-02-18 20:25:26 +0530

    ok

--- portjs.js
+++ portjs.js
@@ -41,6 +41,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     activateLink();
 });
+
+
 document.addEventListener('DOMContentLoaded', function () {
     // Select the inner container for profiles
     const profileContainer = document.querySelector('.profiles-container');
@@ -56,3 +58,44 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add class to start the marquee animation
     profileContainer.classList.add('in-view');
   });
+
+  
+  let menuIcon=document.querySelector('#menu-icon');
+  let navbar =document.querySelector('.navbar');
+
+  menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+  }
+
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('header nav a');
+  window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
+      };
+      
+  });
+  }
+
+  let header=document.querySelector('.header');
+  header.classList.toggle('sticky',window.scrollY>100);
+
+  let menuIcon = document.querySelector('#menu-icon');
+  let nav = document.querySelector('.navbar');
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    nav.classList.toggle('active');
+  }
+
+  
+
+  
